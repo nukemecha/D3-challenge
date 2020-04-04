86b9cdce9b17dda714259be6118dc8fcc6719c7f
Nuke Mecha <49352667+nukemecha@users.noreply.github.com>
2020-04-04 17:25:48 -0500

    final push

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
 // margins and target size
 
-var margin = {top: 30, right: 30, bottom: 30, left: 30};
+var margin = {top: 40, right: 30, bottom: 70, left: 50};
 var width = 800 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
 
@@ -17,22 +17,69 @@ var svg = d3.select("#scatter")
 
 // import csv and commence graphing
 
-d3.csv("assets/data/data.csv", function(data) {
-    // console.log(data);
+d3.csv("assets/data/data.csv").then(function(data) {
+    console.log(data);
 
+    data.forEach(function(d) {
+        d.poverty = +d.poverty;
+        d.obesity = +d.obesity;
+    });
 // setup x-axis
 
-    var xAxis = 
+    var x = d3.scaleLinear()
+        .domain([d3.min(data, function(d) { return d.poverty; }) - 2, 
+            d3.max(data, function(d) { return d.poverty; }) + 2 ])
+        .range([ 0, width ]);
+    
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
 // setup y-axis
 
-    var yAxis = 
+    var y = d3.scaleLinear()
+        .domain([d3.min(data, function(d) { return d.obesity; }) - 2, 
+            d3.max(data, function(d) { return d.obesity; }) + 2 ])
+        .range([ height, 0]);
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
 // render scatter circles
 
     svg.append("g")
         .selectAll("dot")
-        .data()
-})
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function (d) { return x(d.poverty); } )
+            .attr("cy", function (d) { return y(d.obesity); } )
+            .attr("r", 18)
+            .style("fill", "#69b3a2");
+    svg.append("g")
+        .selectAll("text")
+        .data(data)
+            .enter()
+            .append("text")
+            .attr("x", function (d) { return x(d.poverty) - 10; } )
+            .attr("y", function (d) { return y(d.obesity) + 5; } )
+            .text( d => d.abbr);
+
+// axes labels
+
+    svg.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top })`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .text("Poverty (%)");
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2) )
+        .attr("dy", "1em")
+        .text("Obesity (%)")
+                
+});
 
 
